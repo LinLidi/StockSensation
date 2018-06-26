302e8773ba0c1beccf71eb37f4b35d6ed7d63fe4
Lidi Lin <ldlin553@gmail.com>
2018-06-26 23:54:00 +0800

    remove useless

--- stockdata/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-import jieba
-# Create your models here.
-class BlogsPost(models.Model):
-    title = models.CharField(max_length = 150)  # 博客标题
-    body = models.TextField()                   # 博客正文
-    timestamp = models.DateTimeField() 
