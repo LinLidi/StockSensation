d87465cda8da687b596b83b84195536a94eea86c
Lidi Lin <ldlin553@gmail.com>
2018-07-01 22:40:26 +0800

    When Renaming

--- StockSensation/urls.py
+++ StockSensation/urls.py
@@ -1,38 +1,38 @@
 """stock URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+    
+    The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
+    Examples:
+    Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
+    Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+    Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+    """
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from stockdata import views as stockdata_views
+from StockVisualData import views as StockVisualData_views
 
 
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^$', stockdata_views.home, name='home'),
-    url(r'^stockKLine/$', stockdata_views.stockKLine, name='stockKline'),
-
-    url(r'^wordcloud/$', stockdata_views.wordcloud, name='wordcloud'),
-    url(r'^wordcloudResult/$', stockdata_views.wordcloudResult, name='wordcloudResult'),
-    
-    url(r'^dicopinion/$', stockdata_views.dicopinion, name='dicopinion'),
-    url(r'^dicopinionResult/$', stockdata_views.dicopinionResult, name='dicopinionResult'),
-
-    url(r'^nbopinion/$', stockdata_views.nbopinion, name='nbopinion'),
-    url(r'^nbopinionResult/$', stockdata_views.nbopinionResult, name='nbopinionResult'),
-
-]
+               path('admin/', admin.site.urls),
+               url(r'^$', StockVisualData_views.home, name='home'),
+               url(r'^stockKLine/$', StockVisualData_views.stockKLine, name='stockKline'),
+               
+               url(r'^wordcloud/$', StockVisualData_views.wordcloud, name='wordcloud'),
+               url(r'^wordcloudResult/$', StockVisualData_views.wordcloudResult, name='wordcloudResult'),
+               
+               url(r'^dicopinion/$', StockVisualData_views.dicopinion, name='dicopinion'),
+               url(r'^dicopinionResult/$', StockVisualData_views.dicopinionResult, name='dicopinionResult'),
+               
+               url(r'^nbopinion/$', StockVisualData_views.nbopinion, name='nbopinion'),
+               url(r'^nbopinionResult/$', StockVisualData_views.nbopinionResult, name='nbopinionResult'),
+               
+               ]
--- StockSensation/wsgi.py
+++ StockSensation/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for stock project.
+WSGI config for StockSensation project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stock.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "StockSensation.settings")
 
 application = get_wsgi_application()
