d51ce54b5f4a4f12e6018af6771fc3bc2e6178cb
linlidi <ldlin553@gmail.com>
2018-09-19 23:01:35 +0800

    try tencent npl api

--- StockVisualData/views.py
+++ StockVisualData/views.py
@@ -25,6 +25,72 @@ positiveWord = ['向上', '上涨', '涨', '涨停', '高涨', '底', '底部', 
 negativeWord = ['向下', '下跌', '跌', '跌停', '低迷', '顶', '顶部', '空袭', '跳水', '减仓', '减持', '卖出', '卖', '空', '清仓', '暴跌', '亏', '阴跌','拖累', '利空', '考验', '新低', '跌破', '熊', '熊市', '套', '回撤', '垃圾', '哭', '退', '减', '重挫', '平仓', '破灭', '崩', '绿','韭菜', '悲催', '崩溃', '下滑', '拖累', '弱']
 neutralWord = ['震荡', '休养', '休养生息', '谨慎', '观望', '平稳', '过渡', '盘整']
 
+import hashlib
+import time
+import random
+import string
+from urllib.parse import quote
+import requests
+# import md5sign
+
+def curlmd5(src):
+    m = hashlib.md5(src.encode('UTF-8'))
+    return m.hexdigest().upper()
+
+# def get_params(plus_item):
+#     # 请求时间戳（秒级），用于防止请求重放（保证签名5分钟有效）  
+#     t = time.time()
+#     time_stamp=str(int(t))
+#     # 请求随机字符串，用于保证签名不可预测  
+#     nonce_str = ''.join(random.sample(string.ascii_letters + string.digits, 10))  
+#     # 应用标志，这里修改成自己的id和key  
+#     app_id = '11066xxxxx'  
+#     app_key = 'Wz0bsFWuNGGxxxxx'
+#     params = {'app_id':app_id,  
+#               'question':plus_item,
+#               'time_stamp':time_stamp,
+#               'nonce_str':nonce_str,
+#               'session':'10000'
+#              }
+#     sign_before = ''
+#     # 要对key排序再拼接  
+#     for key in sorted(params):
+#         # 键值拼接过程value部分需要URL编码，URL编码算法用大写字母，例如%E8。quote默认大写。  
+#         sign_before += '{}={}&'.format(key,quote(params[key], safe=''))  
+#     # 将应用密钥以app_key为键名，拼接到字符串sign_before末尾  
+#     sign_before += 'app_key={}'.format(app_key)
+#     # 对字符串sign_before进行MD5运算，得到接口请求签名  
+#     sign = curlmd5(sign_before) 
+#     params['sign'] = sign
+#     return params
+
+ 
+ 
+# def get_content(plus_item):    
+#     # 聊天的API地址    
+#     url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textpolar"      
+#     # 获取请求参数  
+#     plus_item = plus_item.encode('utf-8')  
+#     payload = md5sign.get_params(plus_item)    
+#     # r = requests.get(url,params=payload)    
+#     r = requests.post(url,data=payload)    
+#     return r.json()["data"]["answer"]  
+# if __name__ == '__main__':    
+#     while True:    
+#         comment = input('我：')   
+#         if comment == 'q':    
+#             break    
+#         answer=get_content(comment)    
+#         print('机器人：'+answer) 
+def tx_npl(textstring):
+    url = "https://api.ai.qq.com/fcgi-bin/nlp/nlp_textpolar"
+    plus_item = plus_item.encode('utf-8')
+    payload = md5sign.get_params(plus_item)
+    r = requests.post(url,data=payload)
+    return r.json()
+
+
+
 def index(request):
     return render(request,'index.html')
 def dash_index(request):
@@ -153,6 +219,7 @@ def dicopinionResult(request):
                 if int(gotTitle[i][3]) == dateCount[j][0] and int(gotTitle[i][4]) == dateCount[j][1]:
                     dateCount[j][5] += 1
                     segList = list(jieba.cut(gotTitle[i][1], cut_all=True))
+                    print(gotTitle[])
                     for eachItem in segList:
                         if eachItem != ' ':
                             if eachItem in positiveWord:
